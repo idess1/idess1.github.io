0283c097211378bda232de5117816c35a74e6142
Idess Lee <idess@uw.edu>
2020-08-20 23:50:10 -0700

    add script src

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2020 © Idess Lee
+ */
+"use strict";
+
+(function() {
+
+  /**
+   * Add a function that will be called when the window is loaded.
+   */
+  window.addEventListener("load", init);
+
+  function init() {}
+
+  /** ------------------------------ Helper Functions  ------------------------------ */
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /**
+   * Returns a new element with the given tag name.
+   * @param {string} tagName - HTML tag name for new DOM element.
+   * @returns {object} New DOM object for given HTML tag.
+   */
+  function gen(tagName) {
+    return document.createElement(tagName);
+  }
+
+})();
